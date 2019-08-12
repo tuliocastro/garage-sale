ccf1d12a98b91d39807d0e4bd6d5f68688797bb5
Tulio Castro <tcastro@fexco.com>
2019-08-12 16:51:41 +0100

    v1

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ export default class App extends Component {
       { name: "Living room Coffee Table", description: "Base color: black, Size: 90x46 cm", sold: false, storeValue: 56.00, value: 25.00, category: "Home", images: ["coffee_table.webp", "coffee_table2.webp"] },
       { name: "2x Pouffe", description: "The cover is easy to clean, is removable and can be machine washed. ", sold: false, storeValue: 130.00, value: 80.00, category: "Home", images: ["pouffe1.webp", "pouffe2.webp"] },
       { name: "4x Lamp Shade", sold: true, storeValue: 25.00, value: 10.00, category: "Home", images: ["lampshade.png"] },
-      { name: "Office Chair", sold: false, storeValue: 24.00, value: 12.00, category: "Home", images: ["chair.png"] },
+      { name: "Office Chair", sold: false, storeValue: 25.00, value: 13.00, category: "Home", images: ["chair.png"] },
       { name: "Vacuum Cleaner", description: "POLTI Forzaspira C110 Plus (A ‐ 80 dB)", sold: true, storeValue: 71.99, value: 35.00, category: "Home", images: ["vacumcleaner.jpg"] },
       { name: "Abs Roller", sold: false, storeValue: 10.00, value: 5.00, category: "Gym", images: ["absroller.png"] },
-      { name: "Massage Roller", sold: false, storeValue: 9.00, value: 4.50, category: "Gym", images: ["massage_roller.png"] },
+      { name: "Massage Roller", sold: false, storeValue: 10.00, value: 5, category: "Gym", images: ["massage_roller.png"] },
       { name: "Gym Carpet 8x tiles", sold: false, storeValue: 45.00, value: 25.00, category: "Gym", images: [""] },
       { name: "Dumbbel Set 50kg Max + 2 bars", description: "Used for only 2 months", sold: false, storeValue: 60.00, value: 33.00, category: "Gym", images: ["dumbbell1.png", "dumbbell2.png"] },
       { name: "Vida XL. Flywheel ", description: "Used for only 2 months, brand new with Computer functions. Bottle holder and belt drive", sold: false, storeValue: 165.00, value: 92.00, category: "Gym", images: ["byke.png"] },
@@ -28,8 +28,21 @@ export default class App extends Component {
     return (
       <div className="App" >
         <header className="App-header">
-          Selling
-      </header>
+
+          <div className="logo">
+            <img src="imgs/garage-sale.svg" alt="Garage Sale" />
+            <label className="title">Tulio & Vanessa <span>Garage Sale</span></label>
+          </div>
+
+          <div className="intro">
+
+            <p>Hi Guys, these are the items that we are selling before we move.</p>
+
+            <p>All of them are up to 50% of what we paid</p>
+
+            <p>If you want any of those, please call us at <span>Vanessa: <b className="badge">083 053 7207</b> or <span>Tulio: <b className="badge">083 387 5787</b></span> </span></p>
+          </div>
+        </header>
 
         <section className="Filter">
 
--- src/product/Product.js
+++ src/product/Product.js
@@ -3,32 +3,70 @@ import './Product.scss';
 
 export default class Product extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            showDetail: false
+        }
+    }
+
+    toggleDetail(product) {
+        console.log("SHOWING");
+        product.showDetail = !product.showDetail;
+        // console.log(product);
+        // this.props.product.showDetail = !this.props.product.showDetail;
+        this.setState({ showDetail: product.showDetail });
+    }
+
+    close() {
+        this.setState({ showDetail: false });
+    }
+
     render() {
 
         const product = this.props.product;
 
         return (
 
-            <div key={this.props.key} className={"Product " + (this.props.product.sold ? "sold" : "")} >
+            <div>
+                <div key={this.props.key} className={"Product " + (product.sold ? " sold" : "")} onClick={this.toggleDetail.bind(this, product)} >
 
-                <span className="sold-tape">sold</span>
+                    <span className="sold-tape">sold</span>
 
-                <label className="title">{this.props.product.name}</label>
+                    <label className="title">{this.props.product.name}</label>
 
-                <div className="thumbnail">
+                    <div className="thumbnail">
 
-                    <img src={'imgs/' + this.props.product.images[0]} alt="img" />
+                        <img src={'imgs/' + this.props.product.images[0]} alt="img" />
+
+                        <p className="description">{this.props.product.description}</p>
+
+                    </div>
+
+                    <div className="prices">
+                        <label className="paid">We paid: <span>€{product.storeValue}</span></label>
+                        <label className="now">Selling for: <span>€{product.value}</span></label>
+                        <label className="percentage">{(product.value * 100 / product.storeValue).toFixed(0)}</label>
+                    </div>
 
                 </div>
 
-                <p className="description">{this.props.product.description}</p>
 
-                <div className="prices">
-                    <label className="store">We paid: <span>€{product.storeValue}</span>.00</label>
-                    <label className="our">Selling for: <span>€{product.value}</span>.00</label>
+                <div className={"thumbnails" + (this.state.showDetail ? " show-detail" : "")}>
+
+                    <div className="button-close" onClick={this.toggleDetail.bind(this, product)}>x</div>
+
+                    <div className="images">
+                        {
+                            product.images.map((image, key) =>
+                                <img src={"imgs/" + image} key={key} alt="thumbnail" />
+                            )
+                        }
+                    </div>
+
                 </div>
+
             </div>
-
         );
 
     }
