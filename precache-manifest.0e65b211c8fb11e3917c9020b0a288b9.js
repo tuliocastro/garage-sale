ce6cab182918019147e2e3e0870aeeebb7c9f314
Tulio Castro <tcastro@fexco.com>
2019-08-12 17:15:54 +0100

    Updates

---
+++ precache-manifest.0e65b211c8fb11e3917c9020b0a288b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763b635c078b36b16cf364023f899e2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b3cf1949bc02670246e",
+    "url": "/static/css/main.a4737905.chunk.css"
+  },
+  {
+    "revision": "9c5ca2f4a5e34b4fd994",
+    "url": "/static/js/2.be491123.chunk.js"
+  },
+  {
+    "revision": "7b3cf1949bc02670246e",
+    "url": "/static/js/main.787b9bd2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
