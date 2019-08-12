1e8d59df2f1df4bb510e160bae2db456e425d325
Tulio Castro <tcastro@fexco.com>
2019-08-12 16:53:55 +0100

    Updates

---
+++ precache-manifest.f9550fcb0a286429d10cb7aee55bd203.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ec6ac9d6a647cced8c37348e74d297",
+    "url": "/garage-sale/index.html"
+  },
+  {
+    "revision": "93dae03dcaea445d32f7",
+    "url": "/garage-sale/static/css/main.a4737905.chunk.css"
+  },
+  {
+    "revision": "9c5ca2f4a5e34b4fd994",
+    "url": "/garage-sale/static/js/2.be491123.chunk.js"
+  },
+  {
+    "revision": "93dae03dcaea445d32f7",
+    "url": "/garage-sale/static/js/main.307a8be9.chunk.js"
+  },
+  {
+    "revision": "89b2342c1769917d3ace",
+    "url": "/garage-sale/static/js/runtime~main.7454f598.js"
+  }
+]);
