3afbf81b61d0b598cde99671f03174da00c89315
Tulio Castro <tcastro@fexco.com>
2019-08-12 12:40:56 +0100

    images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import Product from './product/Product';
 
 
@@ -8,17 +8,18 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.products = [
-      { name: "Bathroom shelving unit with cabinet", sold: true, storeValue: 54.00, value: 25.00 },
-      { name: "Living room Coffee Table", sold: false, storeValue: 56.00, value: 25.00 },
-      { name: "2x Pouffe", sold: false, storeValue: 130.00, value: 60.00 },
-      { name: "4x Lamp Shade", sold: true, storeValue: 25.00, value: 10.00 },
-      { name: "Office Chair", sold: false, storeValue: 24.00, value: 12.00 },
-      { name: "Vacuum Cleaner", description: "POLTI Forzaspira C110 Plus (A ‐ 80 dB)", sold: true, storeValue: 71.99, value: 35.00 },
-      { name: "Abs Roller", sold: false, storeValue: 10.00, value: 5.00 },
-      { name: "Massage Roller", sold: false, storeValue: 9.00, value: 4.50 },
-      { name: "Gym Carpet 8x tiles", sold: false, storeValue: 45.00, value: 25.00 },
-      { name: "Dumbbel Set 50kg Max + 2 bars", sold: false, storeValue: 60.00, value: 33.00 },
-      { name: "Vida XL. Flywheel ", description: "Computer functions. Bottle holder and belt drive", sold: false, storeValue: 165.00, value: 92.00 },
+      { name: "Bathroom shelving unit with cabinet", description: "Size: 40x27x134 cm", sold: true, storeValue: 56.00, value: 25.00, category: "Home", images: ["bath_shelving1.webp", "bath_shelving2.webp"] },
+      { name: "Living room Coffee Table", description: "Base color: black, Size: 90x46 cm", sold: false, storeValue: 56.00, value: 25.00, category: "Home", images: ["coffee_table.webp", "coffee_table2.webp"] },
+      { name: "2x Pouffe", description: "The cover is easy to clean, is removable and can be machine washed. ", sold: false, storeValue: 130.00, value: 80.00, category: "Home", images: ["pouffe1.webp", "pouffe2.webp"] },
+      { name: "4x Lamp Shade", sold: true, storeValue: 25.00, value: 10.00, category: "Home", images: ["lampshade.png"] },
+      { name: "Office Chair", sold: false, storeValue: 24.00, value: 12.00, category: "Home", images: ["chair.png"] },
+      { name: "Vacuum Cleaner", description: "POLTI Forzaspira C110 Plus (A ‐ 80 dB)", sold: true, storeValue: 71.99, value: 35.00, category: "Home", images: ["vacumcleaner.jpg"] },
+      { name: "Abs Roller", sold: false, storeValue: 10.00, value: 5.00, category: "Gym", images: ["absroller.png"] },
+      { name: "Massage Roller", sold: false, storeValue: 9.00, value: 4.50, category: "Gym", images: ["massage_roller.png"] },
+      { name: "Gym Carpet 8x tiles", sold: false, storeValue: 45.00, value: 25.00, category: "Gym", images: [""] },
+      { name: "Dumbbel Set 50kg Max + 2 bars", description: "Used for only 2 months", sold: false, storeValue: 60.00, value: 33.00, category: "Gym", images: ["dumbbell1.png", "dumbbell2.png"] },
+      { name: "Vida XL. Flywheel ", description: "Used for only 2 months, brand new with Computer functions. Bottle holder and belt drive", sold: false, storeValue: 165.00, value: 92.00, category: "Gym", images: ["byke.png"] },
+      { name: "Pilates ball ", description: "With inflatter", sold: false, storeValue: 8.00, value: 5.00, category: "Gym", images: ["pilatesball.png"] },
     ];
   }
 
@@ -34,10 +35,10 @@ export default class App extends Component {
 
         </section>
 
-        <section className="Products">
+        <section className="products">
           {
-            this.products.map((item, key) => 
-              <Product product={item} key={key}/>
+            this.products.map((item, key) =>
+              <Product product={item} key={key} />
             )
           }
         </section>
--- src/product/Product.js
+++ src/product/Product.js
@@ -5,16 +5,28 @@ export default class Product extends Component {
 
     render() {
 
+        const product = this.props.product;
+
         return (
 
-            <div key={this.props.key} className="Product" >
+            <div key={this.props.key} className={"Product " + (this.props.product.sold ? "sold" : "")} >
+
+                <span className="sold-tape">sold</span>
+
+                <label className="title">{this.props.product.name}</label>
 
                 <div className="thumbnail">
 
+                    <img src={'imgs/' + this.props.product.images[0]} alt="img" />
+
                 </div>
 
-                <label>{this.props.product.name}</label>
+                <p className="description">{this.props.product.description}</p>
 
+                <div className="prices">
+                    <label className="store">We paid: <span>€{product.storeValue}</span>.00</label>
+                    <label className="our">Selling for: <span>€{product.value}</span>.00</label>
+                </div>
             </div>
 
         );
